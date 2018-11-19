7afc92a1c87d9bb65b43f5e1c19778efcf771b9b
indu127pal <ipal127@gmail.com>
2018-11-19 16:46:09 +0530

    Added LottieAnimatedExample for all json animations in react-native

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import LottieAnimatedExample from './src/LottieAnimatedExample';
+
+type Props = {};
+export default class App extends Component<Props> {
+  
+  render() {
+    return (
+      <LottieAnimatedExample />
+    );
+  }
+}
+
